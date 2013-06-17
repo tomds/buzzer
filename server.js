907c31f0cabc2e934a3e376f5d89db77258cfa1f
Tom de Simone <mail@tomandpete.co.uk>
2013-06-17 21:03:46 +0100

    serve index.html from templates rather than public

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ var engines = require('consolidate');
 var app = express();
 
 app.engine('html', engines.hogan);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-    res.render('index');
+    res.render('index', {title: 'test'});
 });
 
 app.listen(3000);
